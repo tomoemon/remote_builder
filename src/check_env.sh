acea16fe349f23797a77c811e47cf90a37285106
tomoemon <bach48+github_tomoemon@gmail.com>
2018-10-04 20:03:39 +0900

    bash のバージョンチェックを追加

--- src/check_env.sh
+++ src/check_env.sh
@@ -22,6 +22,11 @@ command_exists docker-machine "$DOCKER_MACHINE_BIN" https://docs.docker.com/mach
 command_exists watchmedo "$WATCHMEDO_BIN" https://github.com/gorakhargosh/watchdog#installation
 command_exists rsync "$RSYNC_BIN" ""
 
+if ! (bash --version | grep "version 4" >/dev/null); then
+    echo "check bash(version 4)...\e[31mnot found\e[m"
+    result=1
+fi
+
 if [ "$result" -ne 0 ]; then
     echo -e "\nCheck failed. Install the commands above before you start."
     exit 1
