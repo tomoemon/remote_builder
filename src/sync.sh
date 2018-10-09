9a861a6f2d97f863070bdf1477ccb71be9390fcd
tomoemon <bach48+github_tomoemon@gmail.com>
2018-10-09 11:45:14 +0900

    最終ファイル変更時刻、最終ファイル同期時刻をナノ秒単位で管理

--- src/sync.sh
+++ src/sync.sh
@@ -5,11 +5,13 @@ set -eu
 script_dir_path=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 . "$script_dir_path/init_script.sh"
 
+DATE_FORMAT="+%s.%N"
+
 function sync_all {
     set -eu
 
     do_ssh "mkdir -p ${SYNC_TO}"
-    date +%s > /tmp/last_rsync_start_time.txt
+    date "${DATE_FORMAT}" > /tmp/last_rsync_start_time.txt
     eval "$RSYNC_BIN" -aui $SYNC_OPTION "${SYNC_FROM}" ${LOGIN_USERNAME}@${REMOTE_HOST}:${SYNC_TO} | cut -d' ' -f2
 }
 
@@ -18,23 +20,27 @@ function sync_loop {
 
     trap 'kill $(jobs -p)' EXIT
 
-    do_ssh "mkdir -p ${SYNC_TO}"
-
     # ファイル更新の監視を開始する
+    echo "0" > /tmp/latest_update_time.txt
+
     "$WATCHMEDO_BIN" shell-command -R -W \
-        --command 'date +%s > /tmp/latest_update_time.txt; echo ${watch_src_path} was changed' ${SYNC_FROM} \
+        --command "date ${DATE_FORMAT}"'> /tmp/latest_update_time.txt; echo ${watch_src_path} was changed' ${SYNC_FROM} \
         -i "${IGNORE_CHANGE}" &
+
+    do_ssh "mkdir -p ${SYNC_TO}"
 
     while :
     do
         update_time=$(cat /tmp/latest_update_time.txt)
         last_rsync_time=$(cat /tmp/last_rsync_start_time.txt)
 
+        updated=$(echo "$update_time" "$last_rsync_time" | awk -e '{ printf("%d",$1>$2) }')
         # 前回の rsync 開始時以降にファイル更新が発生したら
-        if [ "$update_time" -gt "$last_rsync_time" ] ; then
+        if [ "$updated" -eq 1 ]; then
             # 前回の rsync 開始時から interval 秒経過したら
-            current_time=$(date +%s)
-            if [ $(($current_time - $last_rsync_time)) -ge $SYNC_INTERVAL ] ; then
+            current_time=$(date "${DATE_FORMAT}")
+            interval=$(echo "$current_time" "$last_rsync_time" "$SYNC_INTERVAL" | awk -e '{ printf("%d",($1-$2)>=$3) }')
+            if [ "$interval" -eq 1 ]; then
                 echo $current_time > /tmp/last_rsync_start_time.txt
                 eval "$RSYNC_BIN" -aui $SYNC_OPTION "${SYNC_FROM}" ${LOGIN_USERNAME}@${REMOTE_HOST}:${SYNC_TO} | cut -d" " -f2 | tee "${SYNC_LOG}"
                 on_sync
